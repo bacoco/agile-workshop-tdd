c148b105987c175b4e6af7daa9087ae0a1de2f09
Pedro Rabello <pedrommrabello@gmail.com>
2015-07-08 10:17:17 -0300

    DezOuVintePorcentoTest ficou mais correto, preciso mostrar a classe sendo testada no teste.

--- tests/br/com/caelum/tdd/exercicio1/DezOuVintePorcentoTest.java
+++ tests/br/com/caelum/tdd/exercicio1/DezOuVintePorcentoTest.java
@@ -9,16 +9,19 @@ public class DezOuVintePorcentoTest {
 
 	private Funcionario funcionario;
 	
+	DezOuVintePorcento dezOuVintePorcento;
+	
 	private final static double DELTA = 0.00000001;
 	
 	@Before
 	public void setUp() throws Exception {
+		dezOuVintePorcento = new DezOuVintePorcento();
 	}
 
 	@Test
 	public void testaSalarioMenorOuIgualA3000() {
 		funcionario = new FuncionarioBuilder().comCargo(Cargo.DESENVOLVEDOR).comSalarioBaseDe(3000).build();
-		double calcula = funcionario.getSalarioCalculado();
+		double calcula = dezOuVintePorcento.calcula(funcionario);
 		
 		assertEquals(3000 * 0.9, calcula, DELTA);
 	}
@@ -26,7 +29,8 @@ public class DezOuVintePorcentoTest {
 	@Test
 	public void testaSalarioMaiorQue3000(){
 		funcionario = new FuncionarioBuilder().comCargo(Cargo.DESENVOLVEDOR).comSalarioBaseDe(3100).build();
-		double calcula = funcionario.getSalarioCalculado();
+		
+		double calcula = dezOuVintePorcento.calcula(funcionario);
 		
 		assertEquals(3100 * 0.8, calcula, DELTA);
 	}
